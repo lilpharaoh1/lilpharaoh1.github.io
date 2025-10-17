4dba722731ca64640bc5c7e2186834cd03ad5136
lilpharaoh1 <67148617+lilpharaoh1@users.noreply.github.com>
2025-10-17 10:32:39 +0000

    Deploying to gh-pages from @ lilpharaoh1/lilpharaoh1.github.io@4f0f7d6649d1d6aadbc49a451a927fcd240af41c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-have-started-my-4-year-phd-at-the-university-of-edinburgh",
+          title: 'I have started my 4-year PhD at the University of Edinburgh 🎓',
           description: "",
           section: "News",},{
       id: 'light-theme',
